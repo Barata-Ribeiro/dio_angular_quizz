cf74240cb1e79226c24e79c22d46fcd03d1550ef
João M J Barata Ribeiro <j.mendes1920@gmail.com>
2023-07-27 10:47:11 -0300

    feat: add final logic to buzzfeed style quizz

--- src/app/components/quizz/quizz.component.ts
+++ src/app/components/quizz/quizz.component.ts
@@ -1,4 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import quizz_questions from '../../../assets/data/quizz_questions.json';
+
+interface QuizzOption {
+  id: number;
+  name: string;
+  alias: string;
+}
+
+interface QuizzQuestion {
+  id: number;
+  question: string;
+  options: QuizzOption[];
+}
 
 @Component({
   selector: 'app-quizz',
@@ -8,8 +21,8 @@ import { Component, OnInit } from '@angular/core';
 export class QuizzComponent implements OnInit {
   title: string = '';
 
-  questions: any;
-  questionSelected: any;
+  questions!: QuizzQuestion[];
+  questionSelected!: QuizzQuestion;
 
   answers: string[] = [];
   answerSelected: string = '';
@@ -22,6 +35,49 @@ export class QuizzComponent implements OnInit {
   constructor() {}
 
   ngOnInit(): void {
-    console.log('Iniciou');
+    if (quizz_questions) {
+      this.finished = false;
+      this.title = quizz_questions.title;
+
+      this.questions = quizz_questions.questions;
+      this.questionSelected = this.questions[this.questionIndex];
+
+      this.questionIndex = 0;
+      this.questionMaxIndex = this.questions.length;
+    }
+  }
+
+  playerChoice(value: string) {
+    this.answers.push(value);
+    this.nextQuestion();
+  }
+
+  async nextQuestion() {
+    this.questionIndex += 1;
+    if (this.questionIndex < this.questionMaxIndex)
+      this.questionSelected = this.questions[this.questionIndex];
+    else {
+      let finalAnswer: string = await this.checkResult(this.answers);
+      this.finished = true;
+      this.answerSelected =
+        quizz_questions.results[
+          finalAnswer as keyof typeof quizz_questions.results
+        ];
+    }
+  }
+
+  async checkResult(answers: string[]): Promise<string> {
+    const result = answers.reduce((prev, curr, i, arr) => {
+      let previous = arr.filter((item) => item === prev).length;
+      let current = arr.filter((item) => item === curr).length;
+
+      if (previous > current) {
+        return prev;
+      } else {
+        return curr;
+      }
+    });
+
+    return result;
   }
 }
